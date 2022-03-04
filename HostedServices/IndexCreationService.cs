fa8cffadea74104c76ed483d2d26491f8d1ee111
slorello89 <42971704+slorello89@users.noreply.github.com>
2022-03-04 16:32:56 -0500

    checkpoint 2

---
+++ HostedServices/IndexCreationService.cs
@@ -0,0 +1,30 @@
+﻿using Redis.OM.Skeleton.Model;
+
+namespace Redis.OM.Skeleton.HostedServices;
+
+public class IndexCreationService : IHostedService
+{
+    private readonly RedisConnectionProvider _provider;
+    public IndexCreationService(RedisConnectionProvider provider)
+    {
+        _provider = provider;
+    }
+    
+    /// <summary>
+    /// Checks redis to see if the index already exists, if it doesn't create a new index
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var info = (await _provider.Connection.ExecuteAsync("FT._LIST")).ToArray().Select(x=>x.ToString());
+        if (info.All(x => x != "person-idx"))
+        {
+            await _provider.Connection.CreateIndexAsync(typeof(Person));
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
--- Model/Person.cs
+++ Model/Person.cs
@@ -1,6 +1,28 @@
-﻿namespace Redis.OM.Skeleton.Model;
+﻿using Redis.OM.Modeling;
 
+namespace Redis.OM.Skeleton.Model;
+
+[Document(StorageType = StorageType.Json, Prefixes = new []{"Person"})]
 public class Person
 {
+    // Id Field, also indexed, marked as nullable to pass validation
+    [RedisIdField] [Indexed]public string? Id { get; set; }
+
+    // Indexed for exact text matching
+    [Indexed] public string? FirstName { get; set; }
+    [Indexed] public string? LastName { get; set; }
+
+    //Indexed for numeric matches
+    [Indexed] public int Age { get; set; }
+
+    //Indexed for Full Text matches
+    [Searchable] public string? PersonalStatement { get; set; }
+
+    //Index for membership
+    [Indexed] public string[] Skills { get; set; } = Array.Empty<string>();
+    
+    //Index an object down 1 level
+    [Indexed(CascadeDepth = 1)]
+    public Address? Address { get; set; }
     
 }
